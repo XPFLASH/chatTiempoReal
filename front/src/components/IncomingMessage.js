4a7ba66b0082aa4c93f24744b423105443d0eb7e
blackBuzo2601 <elianpro58@gmail.com>
2024-12-12 03:52:59 -0800

    Estilos para los mensajes entrantes listos, casi listo el chat, solo falta la barra de enviar mensajes

--- front/src/components/IncomingMessage.js
+++ front/src/components/IncomingMessage.js
@@ -1,19 +1,19 @@
-import React from 'react'
-import { horaMes } from '../helpers/horaMes'
+import React from "react";
+import { horaMes } from "../helpers/horaMes";
 
 export const IncomingMessage = ({ msg }) => {
+  return (
+    <div className="contenedorPadreMensajeEntrante">
+      <img
+        src="https://ptetutorials.com/images/user-profile.png"
+        alt="sunil"
+        className="mensajeEntranteAvatar"
+      />
 
-    return (
-        <div className="incoming_msg">
-            <div className="incoming_msg_img">
-                <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil" />
-            </div>
-            <div className="received_msg">
-                <div className="received_withd_msg">
-                    <p>{ msg.mensaje }</p>
-                    <span className="time_date"> { horaMes( msg.createdAt ) }</span>
-                </div>
-            </div>
-        </div>
-    )
-}
+      <div className="mensajeEntranteTextoYHora">
+        <p className="mensajeEntranteTexto">{msg.mensaje}</p>
+        <p className="mensajeEnviadoHora"> {horaMes(msg.createdAt)}</p>
+      </div>
+    </div>
+  );
+};
